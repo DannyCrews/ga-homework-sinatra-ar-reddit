dac88cc957c3635a459f4cd23c52cc30f450d92d
Dan Crews <dancrews@me.com>
2013-10-13 20:30:23 -0400

    Created db and migration NAME=reddit_table

---
+++ db/migrate/20131014002856_reddit_table.rb
@@ -0,0 +1,7 @@
+class RedditTable < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
---
+++ main.rb
@@ -0,0 +1,11 @@
+# Gems required
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pg'
+require 'sinatra/activerecord'
+
+# Define the connection to the database via Activerecord
+set :database, {adapter: 'postgresql',
+								database: 'my_reddit_db',
+								host: 'localhost'}
