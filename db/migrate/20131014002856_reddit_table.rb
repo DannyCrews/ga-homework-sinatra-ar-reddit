576e6443f0fd30b98927cecd2ec5c1868d791541
Dan Crews <dancrews@me.com>
2013-10-14 09:49:33 -0400

    Added Reddit class and reddits table.

--- db/migrate/20131014002856_reddit_table.rb
+++ db/migrate/20131014002856_reddit_table.rb
@@ -1,7 +1,16 @@
 class RedditTable < ActiveRecord::Migration
   def up
+    create_table 		:reddits do |t|
+      t.string   		:title
+      t.text   			:story
+      t.string			:author
+      t.integer  		:up_votes, default: 0
+      t.integer  		:down_votes, default: 0
+      t.timestamps
+    end
   end
 
   def down
+    drop_table reddits
   end
 end
--- main.rb
+++ main.rb
@@ -9,3 +9,9 @@ require 'sinatra/activerecord'
 set :database, {adapter: 'postgresql',
 								database: 'my_reddit_db',
 								host: 'localhost'}
+
+
+# Define the object classes for the databases
+class Reddit < ActiveRecord::Base
+  # has_many :comments
+end 
